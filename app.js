876818e22e1ddb554a3aad4d01db43ef761ad93d
badunk <baduncaduncan@gmail.com>
2013-09-16 16:44:28 -0700

    removed express body parser.  Fixes #21

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ if (cluster.isMaster){
     app.set('port', process.env.PORT || config.port || 3000);
     app.use(express.favicon());
     app.use(express.logger('dev'));
-    app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
 
